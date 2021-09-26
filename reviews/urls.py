4ccfccc5b301c9dd5fd4cb54a26f39044a3d8f42
Bruce Wayne <brucewayne@mail.com>
2021-09-26 14:40:49 -0500

    Cats and stores, store list view with pages

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,8 +1,9 @@
-from .views import index
+from .views import index, StoreListView
 from django.urls import path
 
 app_name = 'reviews'
 
 urlpatterns = [
-	path('', index, name='index')
+	path('', index, name='index'),
+	path('tiendas/', StoreListView.as_view(), name='stores'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,11 @@
+from django.db import models
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render
+from .models import Category, Store, Review
 
 def index(request):
 	return render(request, 'reviews/index.html')
+
+class StoreListView(ListView):
+	model = Store
+	paginate_by = 20
